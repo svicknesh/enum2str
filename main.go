2ec0e0047619fda606b1f1deec0b91e3e8b62214
Vicknesh Suppramaniam <vicknesh.suppramaniam@gmail.com>
2026-06-03 21:40:02 +0800

    fix bounds check off-by-one and empty strList panic

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ type Numeric interface {
 
 // String - returns string value of an input from a map of string list
 func String[T Numeric](input T, strList ...string) (str string) {
+	if len(strList) == 0 {
+		return ""
+	}
+
 	i := int(input)
 
-	if i < 0 || i > len(strList) {
+	if i < 0 || i >= len(strList) {
 		i = 0
 	}
 
